a5372e4a74bb95b971a47f5c97ec10bb003784f8
German Pajarero <germanpajareropino@gmail.com>
2024-04-16 07:43:53 +0200

    Creado constructor y añadida alguna variable.

--- src/RCS.java
+++ src/RCS.java
@@ -1,8 +1,36 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class RCS {
+    private static final Logger LOGGER = Logger.getLogger("serverLogger");
+
+    private String[] argumentos;
     private String modo;
     private int puerto;
     private int max_clientes;
 
+    /**
+     * Constructor de la clase RCS
+     *
+     * @param modo
+     * @param puerto
+     * @param max_clientes
+     */
+
+    public RCS (String modo, int puerto, int max_clientes){
+        this.modo=modo;
+        this.puerto=puerto;
+        this.max_clientes=max_clientes;
+    }
+
+
+    public String[] getArgumentos() {
+        return argumentos;
+    }
+
+    public void setArgumentos(String[] argumentos) {
+        this.argumentos = argumentos;
+    }
 
     public String getModo() {
         return modo;
