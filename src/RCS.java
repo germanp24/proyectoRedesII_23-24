7379667c346f6469b1d20e591ce39ba0dd8c1fd4
German Pajarero <germanpajareropino@gmail.com>
2024-04-16 07:07:34 +0200

    Creadas propiedades que se reciben como argumentos y establecidos sus gets y sets

--- src/RCS.java
+++ src/RCS.java
@@ -1,6 +1,30 @@
 public class RCS {
-    private String serverIP="";
-    private String serverPort="";
+    private String modo;
+    private int puerto;
+    private int max_clientes;
 
 
+    public String getModo() {
+        return modo;
+    }
+
+    public void setModo(String modo) {
+        this.modo = modo;
+    }
+
+    public int getPuerto() {
+        return puerto;
+    }
+
+    public void setPuerto(int puerto) {
+        this.puerto = puerto;
+    }
+
+    public int getMax_clientes() {
+        return max_clientes;
+    }
+
+    public void setMax_clientes(int max_clientes) {
+        this.max_clientes = max_clientes;
+    }
 }
