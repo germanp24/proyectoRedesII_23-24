f1c23c2558a5292e0efc8b18fd357ea554986c20
German Pajarero <germanpajareropino@gmail.com>
2024-05-24 00:31:41 +0200

    Added extra

---
+++ extras/scp_script.bat
@@ -0,0 +1,32 @@
+:: SCRIPT TO SEND SOURCE CODES TO THE INSTANCES QUICKLY
+
+setlocal
+
+:: Keys location
+set KEY_SERVER_PATH=C:\Users\german\ClonedRepos\proyectoRedesII_23-24\keys\german\serverProyecto_keys.pem
+set KEY_CLIENT_PATH=C:\Users\german\ClonedRepos\proyectoRedesII_23-24\keys\german\clientProyecto_keys.pem
+
+:: .java files location
+set RCS=C:\Users\german\ClonedRepos\proyectoRedesII_23-24\src\RCS.java
+set SERVERTHREAD=C:\Users\german\ClonedRepos\proyectoRedesII_23-24\src\ServerThread.java
+set RCC=C:\Users\german\ClonedRepos\proyectoRedesII_23-24\src\RCC.java
+
+:: Server and client public IPs
+set SERVER_IP=184.72.167.0
+set CLIENT_IP=34.228.159.57
+
+:: Remote path where the files will be stored in the instances (I chose /home/ubuntu/proyectoRedes in both instances)
+set REMOTE_PATH=/home/ubuntu/proyectoRedes
+
+:: User for the instances
+set USER=ubuntu
+
+:: Send files via SCP
+:: SERVER
+scp -i %KEY_SERVER_PATH% %RCS% %USER%@%SERVER_IP%:%REMOTE_PATH%
+scp -i %KEY_SERVER_PATH% %SERVERTHREAD% %USER%@%SERVER_IP%:%REMOTE_PATH%
+
+:: CLIENT
+scp -i %KEY_CLIENT_PATH% %RCC% %USER%@%CLIENT_IP%:%REMOTE_PATH%
+
+endlocal
